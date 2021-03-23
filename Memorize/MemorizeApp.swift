f8334c1c30f44d53b0040e0db190059dc349116a
philip-loeffler <philiploeffler@gmail.com>
2021-03-23 10:22:08 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Philip Loeffler on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
